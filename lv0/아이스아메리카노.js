d65776d260e13f4b0d85437a2e9ca5ebb6f04afa
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-10-21 20:10:08 +0900

    Create 아이스아메리카노.js

---
+++ lv0/아이스아메리카노.js
@@ -0,0 +1,13 @@
+// 문제
+// 머쓱이는 추운 날에도 아이스 아메리카노만 마십니다. 아이스 아메리카노는 한잔에 5,500원입니다. 
+// 머쓱이가 가지고 있는 돈 money가 매개변수로 주어질 때, 
+// 머쓱이가 최대로 마실 수 있는 아메리카노의 잔 수와 남는 돈을 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+const solution = (money) => {
+    let result = [];
+  // 나눈 값을 내림하여 할당
+    result[0] = Math.floor(money / 5500)
+  // 나누고 남은 값을 할당
+    result[1] = money % 5500
+    return result
+}
