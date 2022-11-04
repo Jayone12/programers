67e2a09992e0de9ba88c06c65c674f3549b60806
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-11-04 22:25:08 +0900

    Create 두정수사이의합.js

---
+++ lv1/두정수사이의합.js
@@ -0,0 +1,13 @@
+// 문제
+// 두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+// 예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+
+function solution(a, b) {
+    let result = 0;
+    for(let i = Math.min(a,b); i <= Math.max(a,b); i++){
+        result += i
+    }
+    return result
+}
+
+// Math.min() 과 max()는 수를 비교하여 크고 작은 값을 반환한다.
